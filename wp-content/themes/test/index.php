a561467c6a8d7706c22338510a8f81a56bd8acca
JoranBoonstoppel <boonstoppeljoran@gmail.com>
2022-05-25 12:03:57 +0200

    Home page

--- wp-content/themes/test/index.php
+++ wp-content/themes/test/index.php
@@ -2,10 +2,10 @@
 <?php get_header() ?>
 <div class="container">
     <h1><?php  echo  get_the_title() ?></h1>
-    <h2><?php echo get_the_content() ?></h2>
-
-
+    <p><?php echo get_the_content() ?></p>
+    
 </div>
+
 
 
 <?php get_footer() ?>
